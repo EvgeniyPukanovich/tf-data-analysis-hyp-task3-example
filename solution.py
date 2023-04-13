60b207fd8a30c0556dfb3c7728132a9718a07c41
Alex Epik <73494181+EvgeniyPukanovich@users.noreply.github.com>
2023-04-13 13:21:18 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.weightstats import ztest
+
 
 chat_id = 970839957 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+def solution(x: np.array) -> bool:
+    import scipy.stats as st
     alpha = 0.06
-    p = (ztest(x, value = 500,alternative = 'larger')[1])/2
-    return p < alpha # Ваш ответ, True или False
+    t_statistic, p_value = st.ttest_1samp(x, 500, alternative="two-sided")
+    return p_value < 2*alpha and x.mean() > 500
